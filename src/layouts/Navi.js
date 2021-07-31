aa8dea860067381ddd6de9ab2bfae1e4a100c814
Mutlu Kokdemir <kokdemir.mutlu@gmail.com>
2021-08-01 00:35:39 +0300

    If the cart is empty, then doesn't render CartSummary component.

--- src/layouts/Navi.js
+++ src/layouts/Navi.js
@@ -4,13 +4,19 @@ import CartSummary from "./CartSummary";
 import SignedIn from "./SignedIn";
 import SignedOut from "./SignedOut";
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Navi() {
 
+  const { cartItems } = useSelector((state) => state.cart);
   const [isAuthenticated, setIsAuthenticated] = useState(true)
+
+  const history = useHistory()
 
   function handleSignOut() {
     setIsAuthenticated(false)
+    history.push('/')
   }
 
   function handleSignIn() {
@@ -25,7 +31,7 @@ export default function Navi() {
           <Menu.Item name="messages" />
 
           <Menu.Menu position="right">
-            <CartSummary/>
+            {cartItems.length > 0 && <CartSummary/>}
             {isAuthenticated ? <SignedIn signOut={handleSignOut}/> :
               <SignedOut signIn={handleSignIn}/>}
           </Menu.Menu>
