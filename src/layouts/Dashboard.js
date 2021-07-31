2014b0ca5323a04556081bdbc6cf6ecc653e759a
Mutlu Kokdemir <kokdemir.mutlu@gmail.com>
2021-08-01 00:15:22 +0300

    Added routing functionality

--- src/layouts/Dashboard.js
+++ src/layouts/Dashboard.js
@@ -3,11 +3,16 @@ import Categories from "./Categories";
 import Navi from "./Navi";
 import ProductList from "../pages/ProductList";
 import { Grid } from "semantic-ui-react";
+import { Route } from 'react-router'
+import ProductDetail from "../pages/ProductDetail";
+import CartDetail from "../pages/CartDetail";
+import { ToastContainer } from 'react-toastify'
 
 export default function Dashboard() {
   return (
     <div>
       <Navi />
+      <ToastContainer position='bottom-right' autoClose={2000} />
       <Grid>
         <Grid.Row>
           <Grid.Column width={4}>
@@ -15,7 +20,10 @@ export default function Dashboard() {
           </Grid.Column>
 
           <Grid.Column width={12}>
-            <ProductList />
+            <Route exact path='/' component={ProductList} />
+            <Route exact path='/products' component={ProductList} />
+            <Route path='/products/:name' component={ProductDetail} />
+            <Route path='/cart' component={CartDetail} />
           </Grid.Column>
         </Grid.Row>
       </Grid>
